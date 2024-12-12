7d806d138083ebbe651fa2221ea5d754ccd644fe
Ashishlodhi079 <125724751+Ashishlodhi079@users.noreply.github.com>
2024-12-12 16:22:57 +0530

    Update UpdateProduct.js

--- front-end/src/components/UpdateProduct.js
+++ front-end/src/components/UpdateProduct.js
@@ -33,7 +33,7 @@ const getproductdata= async ()=>{
 
 const updateProduct=async ()=>{
 
-    let result = await fetch(`http://localhost:5000/product/${params.id}`,
+    let result = await fetch(`https://edashboard-tdbs.onrender.com/product/${params.id}`,
         {
             method:'PUT',
             body:JSON.stringify({name,price,category,brand}),
