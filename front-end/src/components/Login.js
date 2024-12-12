d7ed1c010a2d54695dd2d041cd23e346c8e721ff
Ashishlodhi079 <125724751+Ashishlodhi079@users.noreply.github.com>
2024-12-12 16:18:55 +0530

    Update Login.js

--- front-end/src/components/Login.js
+++ front-end/src/components/Login.js
@@ -18,7 +18,7 @@ const Login=()=>{
     const login= async ()=>{
         console.warn(email,password);
 
-        let result = await fetch ("http://localhost:5000/login",{
+        let result = await fetch ("https://edashboard-tdbs.onrender.com/login",{
             method:'Post',
             body:JSON.stringify({email,password}),
             headers:{
